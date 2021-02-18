7de329da23234759015f43a55029295d3a8e9dba
gengshaohua <gengshaohua7@163.com>
2021-02-18 14:34:03 +0800

    链表 -- 算法练习

--- linked-list/src/main/java/Solution.java
+++ linked-list/src/main/java/Solution.java
@@ -1,3 +1,4 @@
+
 /**
  * 解决方案
  */
@@ -39,4 +40,209 @@ public class Solution {
         return true;
     }
 
+    /**
+     * 暴力反转法 找到最后一个指针 移除 复制 进行反转
+     * @param head
+     * @return
+     */
+    public ListNode reverse1(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+        ListNode reverse = null;
+        ListNode object = head;
+
+        while(head != null){
+            if(head.next == null){
+                ListNode node = head;
+                ListNode temp = reverse;
+                if(temp == null){
+                    reverse = node;
+                }else{
+                    while (temp.next != null) {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                head = null;
+            }else if(head.next.next == null){
+                ListNode node = head.next;
+                ListNode temp = reverse;
+                if(temp == null){
+                    reverse = node;
+                }else{
+                    while (temp.next != null) {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                head.next = null;
+                head = object;
+            }else{
+                head = head.next;
+            }
+        }
+        return reverse;
+    }
+
+    /**
+     * 反转方式二：从头部取第一个放在反转列表里最后一个位置
+     * @param head
+     * @return
+     */
+    public ListNode reverse2(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+        ListNode reverse = null;
+        while(head != null){
+            if(head.next != null){
+                ListNode next= head.next;
+                ListNode node = head;
+                node.next = reverse;
+                reverse = node;
+                head = next;
+            }else{
+                ListNode node = head;
+                node.next = reverse;
+                reverse = node;
+                head = null;
+            }
+        }
+        return reverse;
+    }
+
+    /**
+     * 检测链表是否是环状
+     * @param head
+     * @return
+     */
+    public boolean cycle(ListNode head){
+        if(head == null || head.next == null)
+            return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * 合并有序链表
+     * @param node1
+     * @param node2
+     * @return
+     */
+    public ListNode merge(ListNode node1, ListNode node2){
+        if(node1 == null ||  node2 == null)
+            return null;
+
+        ListNode newList = null;
+
+        while(node1 != null){
+            if(node2 != null){
+                while(node2 != null){
+                    ListNode temp = newList;
+                    if(node1.val >= node2.val){
+                        ListNode listNode = new ListNode(node2.val);
+                        if(temp == null){
+                            newList = listNode;
+                        }else{
+                            while(temp.next != null){
+                                temp = temp.next;
+                            }
+                            temp.next = listNode;
+                        }
+                        node2 = node2.next;
+                    }else{
+                        ListNode listNode = new ListNode(node1.val);
+                        if(temp == null){
+                            newList = listNode;
+                        }else{
+                            while(temp.next != null){
+                                temp = temp.next;
+                            }
+                            temp.next = listNode;
+                        }
+                        break;
+                    }
+                }
+            }else{
+                ListNode temp = newList;
+                ListNode listNode = new ListNode(node1.val);
+                while(temp.next != null){
+                    temp = temp.next;
+                }
+                temp.next = listNode;
+            }
+            node1 = node1.next;
+        }
+        return newList;
+    }
+
+    /**
+     * 删除链表中指定节点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeIndex(ListNode head, int n){
+        if(head == null)
+            return null;
+        ListNode object = head;
+        int i = 1;
+        while(i < n-1){
+            head = head.next;
+            if (head == null) {
+                return object;
+            }
+            i++;
+        }
+        if(head.next != null){
+            head.next = head.next.next;
+        }
+        return object;
+    }
+
+    /**
+     * 查找中间节点
+     * @param head
+     * @return
+     */
+    public ListNode middleNode(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(5);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(7);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode node5 = new ListNode(1);
+        ListNode node6 = new ListNode(2);
+        ListNode node7 = new ListNode(3);
+        node5.next = node6;
+        node6.next = node7;
+       /* ListNode reverse = solution.reverse1(node1);
+        ListNode reverse2 = solution.reverse2(reverse);*/
+        /*boolean cycle = solution.cycle(node1);*/
+        /*ListNode merge = solution.merge(node1, node5);*/
+        /*ListNode node = solution.removeIndex(node1, 3);*/
+        ListNode node = solution.middleNode(node1);
+        System.out.println(node);
+    }
 }
